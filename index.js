b46079614abfbe1f8552302ae9ec71dbbf7591b8
rtotala <6474355+rtotala@users.noreply.github.com>
2022-09-26 20:44:41 +0530

    1 file modified, 4 added, 2 deleted

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const BLOCK_HEIGHT = 680000;
 const TXN_MAP = {};
 const rootNode = {
   id: BLOCK_HEIGHT,
+  parentsMap: {},
   parents: [],
 };
+let largetTreeTxnId = undefined;
 
 const FETCH_HASH_BY_BLOCK_HEIGHT = 'https://blockstream.info/api/block-height/';
 const FETCH_TXN_BY_BLOCK_HASH = 'https://blockstream.info/api/block/';
@@ -75,6 +77,16 @@ const addAncestor = function (txn, node) {
   }
 };
 
+const getAncestorList = function (node) {
+  let list = [node];
+
+  Object.keys(node.parentsMap).forEach((txn) => {
+    list = list.concat(getAncestorList(node.parentsMap[txn]));
+  });
+
+  return list;
+};
+
 fetchHashForABlock(BLOCK_HEIGHT)
   .then((blockHash) => fetchAllTxnByBlockHash(blockHash))
   .then((txns) => txns.slice(1))
@@ -95,27 +107,42 @@ const printTransactionList = function (txnMap) {
 };
 
 const printAncestorSetforAllTxn = function (txnMap) {
+  let max = -1;
   console.log('--------------------------');
   console.log('Ancestor Set for Every Transaction in the Block' + BLOCK_HEIGHT);
-  console.log(
-    Object.keys(txnMap).forEach((txn) => {
-      console.log('Txn Id : ' + txn);
-      console.log(
-        ['Ancestors: ']
-          .concat(txnMap[txn].parents.map((txn) => txn.id))
-          .join(' ')
-      );
-    })
+
+  Object.keys(txnMap).forEach((txn) => {
+    const list = getAncestorList(txnMap[txn]);
+    txnMap[txn].list = list.slice(1);
+    if (list.length > max) {
+      max = list.length;
+      largetTreeTxnId = txn;
+    }
+    printAncestoryTree(txn);
+  });
+
+  console.log('--------------------------');
+};
+
+const printAncestoryTree = function (txnId, limit) {
+  console.log('Txn Id: ' + txnId + '-> Ancestor Set: ');
+  const list = TXN_MAP[txnId].list.slice(
+    0,
+    limit ? limit : TXN_MAP[txnId].list.length
   );
+  console.log(list.map((list) => list.id).join('\r\n -'));
   console.log('--------------------------');
 };
 
 setTimeout(() => {
-  // printTransactionList(TXN_MAP);
-  // printAncestorSetforAllTxn(TXN_MAP);
+  printTransactionList(TXN_MAP);
+  printAncestorSetforAllTxn(TXN_MAP);
+  console.log('Largest ancestry sets - 10 txns');
+  printAncestoryTree(largetTreeTxnId, 10);
+  console.log('-------------------------');
   printTree(
     rootNode,
     (rootNode) => rootNode.id,
     (rootNode) => rootNode.parents
   );
-}, 5000);
+}, 1000);
